f8296fe0d7f269b723fc150647690df39d8b4156
Mitchell Wyatt <mitchplease02@gmail.com>
2022-12-07 13:30:58 +0000

    almost got solution for challenge 7

---
+++ solutions/challenge-7.js
@@ -0,0 +1,30 @@
+const { dir } = require("console");
+const fs = require("fs/promises");
+
+fs.readFile(`${__dirname}/../data/challenge-7.txt`, "utf-8").then((content) => {
+  const directoryLibrary = {};
+  const path = ["directoryLibrary"];
+
+  const commands = content.split("\n").map((line) => line.split(" "));
+  commands.forEach((line) => {
+    if (line.length === 3) {
+      if (line[2] === "..") path.pop();
+      else if (line[2] !== "/") path.push(line[2]);
+    } else if (line[0] === "dir") eval(path.join(".") + `.${line[1]} = {}`);
+    else if (/\d+/.test(line[0]))
+      eval(path.join(".") + `.${line[1].replace(".", "")} = ${line[0]}`);
+  });
+
+  const targetNumbers = [];
+  const deepValues = (directoryLibrary) => {
+    let sum = 0;
+    for (const entry in directoryLibrary) {
+        console.log(entry)
+      if (typeof directoryLibrary[entry] === "number") sum += directoryLibrary[entry]
+      else deepValues(directoryLibrary[entry])
+    }
+    if (sum <= 100000) targetNumbers.push(sum);
+  };
+  deepValues(directoryLibrary)
+  console.log(targetNumbers.reduce((a, b) => a + b));
+});
